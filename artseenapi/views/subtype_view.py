6d975e4c9d272761486786013866219166549d6c
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-03-15 16:00:14 -0400

    create piece updated

--- artseenapi/views/subtype_view.py
+++ artseenapi/views/subtype_view.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from artseenapi.models import SubType
+from artseenapi.models import SubType, ArtSubRelationship, ArtType
 
 class SubTypeView(ViewSet):
     """Rare subtype view"""
@@ -15,17 +15,12 @@ class SubTypeView(ViewSet):
             Response -- JSON serialized subtype
         """
         try:
-
-            # make connection with server to return single query set where the primary key matches the pk requested by the client and assigns the object instance found to the subtype variable
             subtype = SubType.objects.get(pk=pk)
 
         except SubType.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        # passes the instance stored in subtype through serializer to become a JSON stringified object and assigns it to serializer variable
         serializer = SubTypeSerializer(subtype)
-
-        # returns serializer data to the client as a response. Response body is JSON stringified object of requested data.
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def list(self, request):
@@ -33,17 +28,18 @@ class SubTypeView(ViewSet):
         Returns:
             Response -- JSON serialized list of cities
         """
-        # Make connection with server to retrieve a query set of all cities items requested by client and assign the found instances to the cities variable
         subtypes = SubType.objects.order_by('label')
-        # passes instances stored in subtypes variable to the serializer class to construct data into JSON stringified objects, which it then assigns to variable serializer
+        
+        if "arttypeId" in request.query_params:
+            arttypeId = int(request.query_params['arttypeId'])
+            subtypes = subtypes.filter(subtypes_of_arttype__id=arttypeId)
+
         serializer = SubTypeSerializer(subtypes, many=True)
 
-        # Constructs response and returns data requested by the client in the response body as an array of JSON stringified objects
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class SubTypeSerializer(serializers.ModelSerializer):
     """JSON serializer for game types"""
-    # Converts meta data requested to JSON stringified object using Subtype as model
-    class Meta:  # configuration for serializer
-        model = SubType  # model to use
-        fields = ('id', 'label')  # fields to include
+    class Meta:  
+        model = SubType  
+        fields = ('id', 'label') 
