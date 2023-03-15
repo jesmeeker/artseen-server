826a0d099e3dc257a33c45180f2cc43ba3019c74
Jessica Meeker <113938036+jesmeeker@users.noreply.github.com>
2023-03-15 10:57:11 -0400

    added arttype_view.py

--- artseenapi/views/__init__.py
+++ artseenapi/views/__init__.py
@@ -1,3 +1,4 @@
 from .auth import login_user, register_user
 from .city_view import CityView
 from .piece_view import PieceView
+from .arttype_view import ArtType
--- artseenapi/views/arttype_view.py
+++ artseenapi/views/arttype_view.py
@@ -4,29 +4,29 @@ from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from artseenapi.models import City
+from artseenapi.models import ArtType
 
 
 @authentication_classes([])
 @permission_classes([])
-class CityView(ViewSet):
-    """Rare city view"""
+class ArtTypeView(ViewSet):
+    """Rare arttype view"""
 
     def retrieve(self, request, pk):
-        """Handle GET requests for single city
+        """Handle GET requests for single arttype
         Returns:
-            Response -- JSON serialized city
+            Response -- JSON serialized arttype
         """
         try:
 
-            # make connection with server to return single query set where the primary key matches the pk requested by the client and assigns the object instance found to the city variable
-            city = City.objects.get(pk=pk)
+            # make connection with server to return single query set where the primary key matches the pk requested by the client and assigns the object instance found to the arttype variable
+            arttype = ArtType.objects.get(pk=pk)
 
-        except City.DoesNotExist:
+        except ArtType.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        # passes the instance stored in city through serializer to become a JSON stringified object and assigns it to serializer variable
-        serializer = CitySerializer(city)
+        # passes the instance stored in arttype through serializer to become a JSON stringified object and assigns it to serializer variable
+        serializer = ArtTypeSerializer(arttype)
 
         # returns serializer data to the client as a response. Response body is JSON stringified object of requested data.
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -37,16 +37,16 @@ class CityView(ViewSet):
             Response -- JSON serialized list of cities
         """
         # Make connection with server to retrieve a query set of all cities items requested by client and assign the found instances to the cities variable
-        cities = City.objects.order_by('label')
+        cities = ArtType.objects.order_by('label')
         # passes instances stored in cities variable to the serializer class to construct data into JSON stringified objects, which it then assigns to variable serializer
-        serializer = CitySerializer(cities, many=True)
+        serializer = ArtTypeSerializer(cities, many=True)
 
         # Constructs response and returns data requested by the client in the response body as an array of JSON stringified objects
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-class CitySerializer(serializers.ModelSerializer):
+class ArtTypeSerializer(serializers.ModelSerializer):
     """JSON serializer for game types"""
-    # Converts meta data requested to JSON stringified object using City as model
+    # Converts meta data requested to JSON stringified object using Arttype as model
     class Meta:  # configuration for serializer
-        model = City  # model to use
+        model = ArtType  # model to use
         fields = ('id', 'label')  # fields to include
